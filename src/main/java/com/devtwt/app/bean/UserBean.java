49ce8871f2be8d99c4d91653705d9be9048ba500
joxar <mfpirlo@yahoo.co.jp>
2015-03-28 18:27:23 +0900

    add UserBean

---
+++ src/main/java/com/devtwt/app/bean/UserBean.java
@@ -0,0 +1,54 @@
+package com.devtwt.app.bean;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String userId;
+	private String userName;
+	private String userPassword;
+	
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+
+	public String getUserId() {
+		return userId;
+	}
+
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
