d935f360cf5495c292853b1d18f6b136dcd2c48d
l08084 <l08084@shibaura-it.ac.jp>
2015-07-29 20:20:12 +0900

    ファイルアップロード機能追加-3

--- src/main/java/com/devtwt/app/controller/ArticleController.java
+++ src/main/java/com/devtwt/app/controller/ArticleController.java
@@ -1,99 +1,58 @@
 package com.devtwt.app.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.devtwt.app.bean.FileUploadForm;
 
 @RequestMapping("article")
 @Controller
 public class ArticleController {
+	
+	@Autowired
+	ServletContext context;
 
-    //@Value("${upload.allowableFileSize}")
-   /* private int uploadAllowableFileSize;*/
-
-    // omitted
-
-    // (1)
     @ModelAttribute
     public FileUploadForm setFileUploadForm() {
         return new FileUploadForm();
     }
 
-    // (2)
     @RequestMapping(value = "upload", method = RequestMethod.GET)
     public String uploadForm() {
         return "uploadForm";
     }
 
-    // (3)
-  /*  @RequestMapping(value = "upload", method = RequestMethod.POST)
-    public String upload(@Validated FileUploadForm form,
-            BindingResult result, RedirectAttributes redirectAttributes) {
-
-        if (result.hasErrors()) {
-            return "uploadForm";
-        }
-
-        MultipartFile uploadFile = form.getFile();
-
-        // (4)
-        if (!StringUtils.hasLength(uploadFile.getOriginalFilename())) {
-            result.rejectValue(uploadFile.getName(), "e.xx.at.6002");
-            return "uploadForm";
-        }
-
-        // (5)
-        if (uploadFile.isEmpty()) {
-            result.rejectValue(uploadFile.getName(), "e.xx.at.6003");
-            return "uploadForm";
-        }*/
-
-        // (6)
-       /* if (uploadAllowableFileSize < uploadFile.getSize()) {
-            result.rejectValue(uploadFile.getName(), "e.xx.at.6004",
-                    new Object[] { uploadAllowableFileSize }, null);
-            return "uploadForm";
-        }*/
-
-        // (7)
-        // omit processing of upload.
-
-        // (8)
-        /*redirectAttributes.addFlashAttribute(ResultMessages.success().add(
-                "i.xx.at.0001"));*/
-
-        // (9)
-      /*  System.out.println("DEF");
-        return "redirect:/article/upload?complete";
-    }
-*/
     @RequestMapping(value = "upload", method = RequestMethod.POST,headers=("content-type=multipart/*"))
     public String uploadComplate(@RequestParam MultipartFile file, Model model)throws IOException {
     	
-    	 Path path = Paths.get(System.getProperty("java.io.tmpdir"),
-                 file.getOriginalFilename());
-         file.transferTo(path.toFile());
-         model.addAttribute("fileName", path);
-         System.out.println("ABC");
+    	//imagesディレクトリがなければ作成
+    	File imageDir = new File(context.getRealPath("/") + "/images");
+  
+    	if (!imageDir.exists()) {
+			imageDir.mkdir();
+		}
+    	
+    	//コピー先のパスを取得
+    	Path path = Paths.get(context.getRealPath("/") + "/images",
+                file.getOriginalFilename());
+    	
+    	file.transferTo(path.toFile());
+    	
+        model.addAttribute("fileName", path);
     	
         return "uploadRecv";
     }
-
-    // omitted
-
 }
